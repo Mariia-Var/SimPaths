4be97c9b87142d5962a68dd051c6e4cff8513286
Adrien Morison <adrien.morison@gmail.com>
2026-08-05 14:27:31 +0100

    fix `FemalesWithChildrenByChildAgeCSfilter`
    This filter has two issues:
    - the check it was attempting to perform was incorrectly implemented;
    - the check it was attempting to perform was not the desired one: it
      tried to check that all children are in the given age range, whereas
      checking that any child is in the given range is the desired
      behaviour.
    
    This commit also introduces the new `Filters` "class" which is a
    collection of static methods to easily build predicates on a `Person`.
    This will become most useful when introducing the newer APIs currently
    in development in jasmine.

---
+++ src/main/java/simpaths/data/filters/Filters.java
@@ -0,0 +1,39 @@
+package simpaths.data.filters;
+
+import java.util.function.Predicate;
+
+import simpaths.model.Person;
+import simpaths.model.enums.Gender;
+import simpaths.model.enums.Indicator;
+
+/// Collection of [Person] filters.
+public class Filters {
+    private Filters() {
+    }
+
+    /// Filter the requested gender.
+    public static Predicate<Person> gender(Gender gender) {
+        return p -> p.getDemMaleFlag() == gender;
+    }
+
+    /// Filter male persons.
+    public static Predicate<Person> male() {
+        return gender(Gender.Male);
+    }
+
+    /// Filter female persons.
+    public static Predicate<Person> female() {
+        return gender(Gender.Female);
+    }
+
+    /// Filter given age range (both ends are included).
+    public static Predicate<Person> ageRange(int from, int to) {
+        return p -> p.getDemAge() >= from && p.getDemAge() <= to;
+    }
+
+    /// Filter persons with at least one child in the given age range (both
+    /// ends included).
+    public static Predicate<Person> hasChildInAgeRange(int from, int to) {
+        return p -> p.getBenefitUnit().getIndicatorChildren(from, to) == Indicator.True;
+    }
+}
